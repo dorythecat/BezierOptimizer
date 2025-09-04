eff806793cc782a3efe9c92958d54d4e606df237
dorythecat <pemegomachado@gmail.com>
2025-09-04 21:13:19 +0200

    Made the equation simplifier work over any variable

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-test_equation = ("x * x * x + x * x + 3 * x + 2 * x")
+test_equation = "t * t * t + t * t + 3 * t + 2 * t"
 
 # Simplify a written-out equation into something simpler
-def simplify_equation(equation: str) -> str:
+def simplify_equation(equation: str, variable: str = "t") -> str:
     split_add = equation.split("+")
 
-    least_x_content = 10000
+    least_var_content = 10000 # Something really high so it doesn't interfere with anything
     for i in range(len(split_add)):
         add = split_add[i]
-        x_content = add.count("x")
-        if x_content < least_x_content:
-            least_x_content = x_content
+        var_content = add.count(variable)
+        if var_content < least_var_content:
+            least_var_content = var_content
         split_add[i] = add.strip()
-    if least_x_content > 0:
+    if least_var_content > 0:
         number_terms = dict()
         for i in range(len(split_add)):
-            if split_add[i].strip().count("x * x") == 0:
-                split_add[i] = split_add[i].replace("x", "").replace("*", "").strip()
+            if split_add[i].strip().count(variable + " * " + variable) == 0:
+                split_add[i] = split_add[i].replace(variable, "").replace("*", "").strip()
                 if split_add[i] == "":
                     split_add[i] = "1"
                 number_terms.update({i: int(split_add[i])})
                 continue
-            split_add[i] = split_add[i].replace("x * ", "", least_x_content).strip()
+            split_add[i] = split_add[i].replace(variable + " * ", "", least_x_content).strip()
         number_count = 0
         i_offset = 0
         if len(number_terms) != 0:
@@ -31,12 +31,12 @@ def simplify_equation(equation: str) -> str:
                 if i == max(number_terms.keys()):
                     split_add.append(str(number_count))
     equation = " + ".join(split_add)
-    if least_x_content > 0:
-        for i in range(least_x_content):
+    if least_var_content > 0:
+        for i in range(least_var_content):
             if i == 0:
-                equation = "x * (" + equation + ")"
+                equation = variable + " * (" + equation + ")"
             else:
-                equation = "x * " + equation
+                equation = variable + " * " + equation
     return equation
 
 print(simplify_equation(test_equation))
