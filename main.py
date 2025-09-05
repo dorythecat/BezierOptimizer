7d40c8fe56b00a342d929bd6187c7ba970c1d514
dorythecat <pemegomachado@gmail.com>
2025-09-05 11:09:50 +0200

    Added the ability to change output type

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
+# The type of string to spit out
+# 0 = Human-readable equation
+# 1 = Python equation
+# 2 = MatLab equation
+OUTPUT_TYPE = 0
+
+mul = " * " if OUTPUT_TYPE in [0, 1] else ".*"
+power = "²" if OUTPUT_TYPE == 0 else ("**2" if OUTPUT_TYPE == 1 else ".^2")
+
 # Quadratic Bézier curve (one-dimensional) equation function
 def quad_bezier(p0: float, p1: float, p2: float) -> str:
     p2 = p2 - p1
     p2n = p2 < 0 # is it negative?
     p2 *= -1 if p2n else 1 # Make it positive
-    p2 = (" - " if p2n else " + ") + ("" if p2 == 1 else (str(p2) + " * ")) # Turn to string
+    p2 = (" - " if p2n else " + ") + ("" if p2 == 1 else (str(p2) + mul)) # Turn to string
     if p0 == p1:
-        output = ("" if p1 == 0 else str(p1)) + ("" if p2[3:-3] == "0" else (p2 + "t²"))
+        output = ("" if p1 == 0 else str(p1)) + ("" if p2[3:-3] == "0" else (p2 + "t" + power))
         return "" if output == "0" else output
     # Same steps as above
     p0 = p0 - p1
     p0n = p0 < 0
     p0 *= -1 if p0n else 1
-    p0 = (" - " if p0n else " + ") + ("" if p0 == 1 else (str(p0) + " * "))
+    p0 = (" - " if p0n else " + ") + ("" if p0 == 1 else (str(p0) + mul))
 
     p1 = ("" if p1 == 0 else str(p1)) # Convert to string
     if p2[3:-3] == "0":
-        return p1 + p0 + "(1 - t)²"
+        return p1 + p0 + "(1 - t)" + power
     if p0[3:] == p2[3:]: # Ignore signs
         if p1 == "":
             return ""
-        return ("" if p1 == "1" else ("-" if p1 == "-1" else p1)) + p0[:-3] + "((1 - t)² + t²)"
-    p0 = ((("- " if p0 == " - " else "") + "1") if p0 in [" + ", " - "] and p1 == "" else (p0 + "(1 - t)²"))
-    return p1 + p0 + ("" if p2[3:-3] == "0" else (p2 + "t²"))
+        return ("" if p1 == "1" else ("-" if p1 == "-1" else p1)) + p0[:-3] + "((1 - t)" + power + " + t" + power + ")"
+    p0 = ((("- " if p0 == " - " else "") + "1") if p0 in [" + ", " - "] and p1 == "" else (p0 + "(1 - t)" + power))
+    return p1 + p0 + ("" if p2[3:-3] == "0" else (p2 + "t" + power))
 
 def cubic_bezier(p0: float, p1: float, p2: float, p3: float) -> str:
     b1 = quad_bezier(p0, p1, p2) # Precalculate curve
-    b1 = ("" if b1 == "" else ("(1 - t) * " + "(" + b1 + ")")) # Turn to string
+    b1 = ("" if b1 == "" else ("(1 - t)" + mul + "(" + b1 + ")")) # Turn to string
     # Same as above
     b2 = quad_bezier(p1, p2, p3)
-    b2 = ("" if b2 == "" else ("t * (" + b2 + ")"))
+    b2 = ("" if b2 == "" else ("t" + mul + "(" + b2 + ")"))
     return b1 + ("" if b1 == "" or b2 == "" else " + ") + b2
 
 def n_bezier(points: list[float]) -> str:
@@ -38,14 +47,14 @@ def n_bezier(points: list[float]) -> str:
         case 2:
             sp = sum(points)
             p0 = ("" if points[0] == 0 else (str(points[0]) + " + "))
-            return p0 + ("" if sp == 0 else (("" if sp == 1 else ("-" if sp == -1 else str(sp))) + " * t"))
+            return p0 + ("" if sp == 0 else (("" if sp == 1 else ("-" if sp == -1 else str(sp))) + mul + "t"))
         case 3:
             return quad_bezier(*points)
         case 4:
             return cubic_bezier(*points)
         case _:
             b1 = n_bezier(points[:-1])
-            b1 = ("" if b1 == "" else ("(1 - t) * " + "(" + b1 + ")"))
+            b1 = ("" if b1 == "" else ("(1 - t)" + mul + "(" + b1 + ")"))
             b2 = n_bezier(points[1:])
-            b2 = ("" if b2 == "" else ("t * (" + b2 + ")"))
+            b2 = ("" if b2 == "" else ("t" + mul + "(" + b2 + ")"))
             return b1 + ("" if b1 == "" or b2 == "" else " + ") + b2
