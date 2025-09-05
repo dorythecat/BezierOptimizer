77542a958925a8b9c05dbe4452c69aa0c4c93a13
dorythecat <pemegomachado@gmail.com>
2025-09-05 10:23:05 +0200

    Improved bezier output

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def quad_bezier(p0: float, p1: float, p2: float) -> str:
     p2 *= -1 if p2n else 1 # Make it positive
     p2 = (" - " if p2n else " + ") + ("" if p2 == 1 else (str(p2) + " * ")) # Turn to string
     if p0 == p1:
-        return str(p1) + ("" if p2 == 0 else (p2 + "t²"))
+        output = str(p1) + ("" if p2[3:-3] == "0" else (p2 + "t²"))
+        return "" if output == "0" else output
     # Same steps as above
     p0 = p0 - p1
     p0n = p0 < 0
@@ -16,12 +17,10 @@ def quad_bezier(p0: float, p1: float, p2: float) -> str:
     if p0[3:] == p2[3:]: # Ignore signs
         if p1 == 0:
             return ""
-        return str(p1) + p0[:-3] + "((1 - t)² + t²)"
-    return str(p1) + p0 + "(1 - t)²" + p2 + "t²"
+        return ("" if p1 == 1 else ("-" if p1 == -1 else str(p1))) + p0[:-3] + "((1 - t)² + t²)"
+    return ("" if p1 == 0 else str(p1)) + ((("- " if p0 == " - " else "") + "1") if p0 in [" + ", " - "] and p1 == 0 else (p0 + "(1 - t)²")) + ("" if p2[3:-3] == "0" else (p2 + "t²"))
 
 def cubic_bezier(p0: float, p1: float, p2: float, p3: float) -> str:
     b1 = quad_bezier(p0, p1, p2)
     b2 = quad_bezier(p1, p2, p3)
-    return ("" if b1 == "" else ("(1 - t) * " + "(" + b1 + ") + ")) + ("" if b2 == "" else ("t * (" + b2 + ")"))
-
-print(cubic_bezier(1, 0, -1, 0))
+    return ("" if b1 == "" else ("(1 - t) * " + "(" + b1 + ")")) + ("" if b1 == "" or b2 == "" else " + ") + ("" if b2 == "" else ("t * (" + b2 + ")"))
